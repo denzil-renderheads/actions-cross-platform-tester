9ade04744fd4f857123ff6fea54c990e3a4a186f
denzil-renderheads <59559312+denzil-renderheads@users.noreply.github.com>
2022-07-05 12:46:46 +0200

    Test windows

--- Assets/BehaviourTest_windows.cs
+++ Assets/BehaviourTest_windows.cs
@@ -6,5 +6,8 @@ public partial class BehaviourTest
 {
 #if UNITY_EDITOR_WIN || (UNITY_STANDALONE_WIN && !UNITY_EDITOR)
     public string test = "blah";
-    #endif
+#endif
+#if UNITY_EDITOR_LINUX || (UNITY_STANDALONE_LINUX && !UNITY_EDITOR)
+    public string test = "blah";
+#endif
 }
